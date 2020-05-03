e1fb61d464c2ab87d27ca273e9bd227b0aacf6e3
Akshay Shinde <ednihs.yahska@gmail.com>
2020-05-03 14:53:06 -0700

    initiating client auth

--- src/modules/sign_in_with_apple.ts
+++ src/modules/sign_in_with_apple.ts
@@ -1,6 +1,8 @@
 import fs from 'fs'
 import jwt from 'jsonwebtoken'
 import express, { Request, Response } from 'express';
+import axios from "axios"
+import querystring from "querystring"
 
 const authRoute = express.Router()
 
@@ -29,6 +31,35 @@ const getClientSecret = () => {
 authRoute.get("/auth/apple", (req: Request, res: Response)=>{
     console.log(getClientSecret())
     res.status(200).send(getClientSecret())
+})
+
+authRoute.post("/apple/redirect", (req: Request, res: Response)=>{
+    const clientSecret = getClientSecret()
+    const requestBody = {
+        grant_type: 'authorization_code',
+        code: req.body.code,
+        redirect_uri: "www.meoclocks.com/logged-in",
+        client_id: process.env.CLIENT_ID,
+        client_secret: clientSecret,
+        scope: "name email"
+    }
+
+    axios.request({
+        method: "POST",
+        url: "https://appleid.apple.com/auth/token",
+        data: querystring.stringify(requestBody),
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+       }).then(response => {
+        return res.json({
+         success: true,
+         data: response.data
+        })
+       }).catch(error => {
+        return res.status(500).json({
+         success: false,
+         error: error.response.data
+        })
+    })
 })
 
 export default authRoute
