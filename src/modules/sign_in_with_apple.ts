94d5f33cc81a6afbedbdfe28cefa943743480301
Akshay Shinde <ednihs.yahska@gmail.com>
2020-05-04 14:31:05 -0700

    removing key verification

--- src/modules/sign_in_with_apple.ts
+++ src/modules/sign_in_with_apple.ts
@@ -36,7 +36,7 @@ const getClientSecret = (key: string) => {
     })
     const key1 = "iGaLqP6y-SJCCBq5Hv6pGDbG_SQ11MNjH7rWHcCFYz4hGwHC4lcSurTlV8u3avoVNM8jXevG1Iu1SY11qInqUvjJur--hghr1b56OPJu6H1iKulSxGjEIyDP6c5BdE1uwprYyr4IO9th8fOwCPygjLFrh44XEGbDIFeImwvBAGOhmMB2AD1n1KviyNsH0bEB7phQtiLk-ILjv1bORSRl8AK677-1T8isGfHKXGZ_ZGtStDe7Lu0Ihp8zoUt59kx2o9uWpROkzF56ypresiIl4WprClRCjz8x6cPZXU2qNWhu71TQvUFwvIvbkE1oYaJMb0jcOTmBRZA2QuYw-zHLwQ"
     const key2 = "4dGQ7bQK8LgILOdLsYzfZjkEAoQeVC_aqyc8GC6RX7dq_KvRAQAWPvkam8VQv4GK5T4ogklEKEvj5ISBamdDNq1n52TpxQwI2EqxSk7I9fKPKhRt4F8-2yETlYvye-2s6NeWJim0KBtOVrk0gWvEDgd6WOqJl_yt5WBISvILNyVg1qAAM8JeX6dRPosahRVDjA52G2X-Tip84wqwyRpUlq2ybzcLh3zyhCitBOebiRWDQfG26EH9lTlJhll-p_Dg8vAXxJLIJ4SNLcqgFeZe4OfHLgdzMvxXZJnPp_VgmkcpUdRotazKZumj6dBPcXI_XID4Z4Z3OM1KrZPJNdUhxw"
-    console.log(jwt.verify(token, key))
+    //console.log(jwt.verify(token, key))
     //console.log(jwt.verify(token, key2))
     console.log(token)
     return token
@@ -53,7 +53,6 @@ authRoute.post("/auth/apple", (req: Request, res: Response)=>{
 
 authRoute.post("/apple/redirect", async (req: Request, res: Response)=>{
     const keyResult: AxiosAuthKeyResult = await axios.get("https://appleid.apple.com/auth/keys")
-    console.log(keyResult)
     const key = keyResult.data.keys[0].n
     const clientSecret = getClientSecret(key)
     console.log(req.body)
