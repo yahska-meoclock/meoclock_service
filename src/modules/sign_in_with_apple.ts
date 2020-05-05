7c1963a81a0587dc08c6386e94e2bc93b7baa9d7
Akshay Shinde <ednihs.yahska@gmail.com>
2020-05-04 18:38:43 -0700

    trying node library for apple signin

--- src/modules/sign_in_with_apple.ts
+++ src/modules/sign_in_with_apple.ts
@@ -4,6 +4,8 @@ import express, { Request, Response } from 'express';
 import axios from "axios"
 import querystring from "querystring"
 
+import appleSignin from "apple-signin"
+
 const authRoute = express.Router()
 
 interface AppleAuthKeyCollection {
@@ -53,32 +55,49 @@ authRoute.post("/auth/apple", (req: Request, res: Response)=>{
 })
 
 authRoute.post("/apple/redirect", async (req: Request, res: Response)=>{
-    const clientSecret = getClientSecret()
-    console.log(req.body)
-    const requestBody = {
-        grant_type: 'authorization_code',
-        code: req.body.code,
-        redirect_uri: "www.meoclocks.com/auth/apple",
-        client_id: process.env.CLIENT_ID,
-        client_secret: clientSecret
-    }
-    console.log(requestBody)
-    axios.request({
-        method: "POST",
-        url: "https://appleid.apple.com/auth/token",
-        data: querystring.stringify(requestBody),
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-       }).then(response => {
-        return res.json({
-         success: true,
-         data: response.data
-        })
-       }).catch(error => {
-        return res.status(500).json({
-         success: false,
-         error: error.response.data
-        })
-    })
+    //const clientSecret = getClientSecret()
+    const clientSecret = appleSignin.getClientSecret({
+        clientID: process.env.CLIENT_ID, // identifier of Apple Service ID.
+        teamId: process.env.TEAM_ID, // Apple Developer Team ID.
+        privateKeyPath: process.env.APPLE_PRIVATE_KEY_FILE, // path to private key associated with your client ID.
+        keyIdentifier: process.env.KEY_ID // identifier of the private key.    
+    });
+    const options = {
+        clientID: process.env.CLIENT_ID, // identifier of Apple Service ID.
+        redirectUri: 'https://www.meoclocks.com/apple/redirect', // use the same value which you passed to authorisation URL.
+        clientSecret: clientSecret
+    };
+
+    appleSignin.getAuthorizationToken(req.body.code, options).then((tokenResponse: any) => {
+        console.log(tokenResponse);
+    }).catch((error: Error) => {
+        console.log(Error);
+    });
+    // console.log(req.body)
+    // const requestBody = {
+    //     grant_type: 'authorization_code',
+    //     code: req.body.code,
+    //     redirect_uri: "www.meoclocks.com/auth/apple",
+    //     client_id: process.env.CLIENT_ID,
+    //     client_secret: clientSecret
+    // }
+    // console.log(requestBody)
+    // axios.request({
+    //     method: "POST",
+    //     url: "https://appleid.apple.com/auth/token",
+    //     data: querystring.stringify(requestBody),
+    //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    //    }).then(response => {
+    //     return res.json({
+    //      success: true,
+    //      data: response.data
+    //     })
+    //    }).catch(error => {
+    //     return res.status(500).json({
+    //      success: false,
+    //      error: error.response.data
+    //     })
+    // })
 })
 
 export default authRoute
