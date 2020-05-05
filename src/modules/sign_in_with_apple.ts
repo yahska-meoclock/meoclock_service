089e928a95b7a6c2f342e2e37413347abd060b27
Akshay Shinde <ednihs.yahska@gmail.com>
2020-05-04 18:22:30 -0700

    logging request body

--- src/modules/sign_in_with_apple.ts
+++ src/modules/sign_in_with_apple.ts
@@ -21,7 +21,7 @@ interface AxiosAuthKeyResult {
     data: AppleAuthKeyCollection
 }
 
-const getClientSecret = (key: string) => {
+const getClientSecret = () => {
     const privateKey = fs.readFileSync(process.env.APPLE_PRIVATE_KEY_FILE??"", {encoding:"utf8"});
     const timeNow = Math.floor(Date.now() / 1000);
     const token = jwt.sign(
@@ -30,7 +30,7 @@ const getClientSecret = (key: string) => {
         iat: timeNow,
         exp: timeNow + 15777000,
         aud: 'https://appleid.apple.com',
-        subject: process.env.CLIENT_ID
+        sub: process.env.CLIENT_ID
     }, privateKey, 
     {
         algorithm:"ES256",
@@ -39,11 +39,6 @@ const getClientSecret = (key: string) => {
             kid:process.env.KEY_ID
         }
     })
-
-    const key1 = "iGaLqP6y-SJCCBq5Hv6pGDbG_SQ11MNjH7rWHcCFYz4hGwHC4lcSurTlV8u3avoVNM8jXevG1Iu1SY11qInqUvjJur--hghr1b56OPJu6H1iKulSxGjEIyDP6c5BdE1uwprYyr4IO9th8fOwCPygjLFrh44XEGbDIFeImwvBAGOhmMB2AD1n1KviyNsH0bEB7phQtiLk-ILjv1bORSRl8AK677-1T8isGfHKXGZ_ZGtStDe7Lu0Ihp8zoUt59kx2o9uWpROkzF56ypresiIl4WprClRCjz8x6cPZXU2qNWhu71TQvUFwvIvbkE1oYaJMb0jcOTmBRZA2QuYw-zHLwQ"
-    const key2 = "4dGQ7bQK8LgILOdLsYzfZjkEAoQeVC_aqyc8GC6RX7dq_KvRAQAWPvkam8VQv4GK5T4ogklEKEvj5ISBamdDNq1n52TpxQwI2EqxSk7I9fKPKhRt4F8-2yETlYvye-2s6NeWJim0KBtOVrk0gWvEDgd6WOqJl_yt5WBISvILNyVg1qAAM8JeX6dRPosahRVDjA52G2X-Tip84wqwyRpUlq2ybzcLh3zyhCitBOebiRWDQfG26EH9lTlJhll-p_Dg8vAXxJLIJ4SNLcqgFeZe4OfHLgdzMvxXZJnPp_VgmkcpUdRotazKZumj6dBPcXI_XID4Z4Z3OM1KrZPJNdUhxw"
-    //console.log(jwt.verify(token, key))
-    //console.log(jwt.verify(token, key2))
     console.log(token)
     return token
 }
@@ -58,9 +53,7 @@ authRoute.post("/auth/apple", (req: Request, res: Response)=>{
 })
 
 authRoute.post("/apple/redirect", async (req: Request, res: Response)=>{
-    const keyResult: AxiosAuthKeyResult = await axios.get("https://appleid.apple.com/auth/keys")
-    const key = keyResult.data.keys[0].n
-    const clientSecret = getClientSecret(key)
+    const clientSecret = getClientSecret()
     console.log(req.body)
     const requestBody = {
         grant_type: 'authorization_code',
@@ -69,7 +62,7 @@ authRoute.post("/apple/redirect", async (req: Request, res: Response)=>{
         client_id: process.env.CLIENT_ID,
         client_secret: clientSecret
     }
-
+    console.log(requestBody)
     axios.request({
         method: "POST",
         url: "https://appleid.apple.com/auth/token",
