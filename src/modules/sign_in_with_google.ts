1bfd71a6b970b4247f0d587835a8636cba7b5fdf
Akshay Shinde <s.yahska@gmail.com>
2020-06-07 00:14:21 -0700

    trying various passport methods

--- src/modules/sign_in_with_google.ts
+++ src/modules/sign_in_with_google.ts
@@ -9,16 +9,17 @@ const googleAuth = express.Router()
 googleAuth.get('/google/begin-auth', passport.authenticate("google", {
   scope: ['https://www.googleapis.com/auth/plus.login'],
   session: false
-}));
+}), function(req, res) {
+  console.log("User ", req.user)
+  res.json(req.user);
+});
 
 // GET /auth/google/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
-googleAuth.get('/google/redirect', passport.authenticate("google",{
-  session: false
-}), function(req, res) {
+googleAuth.get('/google/redirect', function(req, res) {
     console.log("User ", req.user)
     res.json(req.user);
 });
