7d2a55ba18224b3c69f56ac26a54f2cb5c33d97b
Akshay Shinde <s.yahska@gmail.com>
2020-06-07 09:50:46 -0700

    Logging code from google sign in

--- src/modules/sign_in_with_google.ts
+++ src/modules/sign_in_with_google.ts
@@ -7,6 +7,7 @@ import CryptoJS from "crypto-js"
 import urlencode from "urlencode"
 import shortid from "shortid"
 import redis from "../connections/redis"
+const querystring = require('querystring');
 
 const googleAuth = express.Router()
 
@@ -49,8 +50,8 @@ googleAuth.get('/google/redirect', async function(req, res) {
     process.env.GOOGLE_CLIENT_SECRET,
     'https://service.meoclocks.com/google/redirect'
   );
-  const {code, state} = req.params
-  console.log("State ", state)
+  const {code, state} = querystring.parse(req.url)
+  console.log("State ", state, " Url ", req.url, " Code ", code)
   const {tokens} = await oauth2Client.getToken(code)
   oauth2Client.setCredentials(tokens); 
   const userInfo = await oauth2Client.userinfo.get()
